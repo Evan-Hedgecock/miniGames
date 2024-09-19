0bdc6e8d4b666ac2b1e2255c33561c0286791fad
Evan <evanjameshedgecock@gmail.com>
2024-09-19 09:32:09 -0800

    Finish double jump and some other movement in swords

---
+++ swords/PlayerMovementData.gd
@@ -0,0 +1,9 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 300.0
+@export var jump_velocity = -350.0
+@export var acceleration = 100
+@export var friction = 100
+@export var air_acceleration = 75
+@export var double_jump_velocity = 0.8
--- swords/scripts/player.gd
+++ swords/scripts/player.gd
@@ -1,16 +1,15 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -350.0
-const ACCELERATION = 100
-const FRICTION = 100
+@export var movement_data: PlayerMovementData
 
 var jumping = false
 var landing = false
+var can_double_jump = false
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var landing_zone: CollisionPolygon2D = %LandingZone
+@onready var coyote_jump_timer: Timer = %CoyoteJumpTimer
+@onready var landing_timer: Timer = %LandingTimer
 
 
 func _physics_process(delta: float) -> void:
@@ -18,9 +17,15 @@ func _physics_process(delta: float) -> void:
 	process_jump()
 	process_movement(delta)
 	process_animations()
-	
+	manage_landing()
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_floor = was_on_floor != is_on_floor() and !is_on_floor()
+	manage_coyote_timer(just_left_floor)
 	
+	landing = !was_on_floor and is_on_floor()
+	
+		
 func process_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -28,46 +33,80 @@ func process_gravity(delta):
 func process_jump():
 	if is_on_floor():
 		jumping = false
+		can_double_jump = true
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
-			jumping = true
+			jump()
+	elif coyote_jump_timer.time_left > 0 and !jumping:
+		if Input.is_action_just_pressed("jump"):
+			jump()
+	elif can_double_jump:
+		if Input.is_action_just_pressed("jump"):
+			jump()
+			can_double_jump = false
 		
 func process_movement(delta):
 	var input_axis = get_input_axis()
 	if input_axis:
-		accelerate(input_axis)
+		if is_on_floor():
+			accelerate(input_axis)
+		else:
+			air_acceleration(input_axis)
+			
 	else:
 		friction()
 		
 func process_animations():
 	var input_axis = get_input_axis()
-	# Use landing_zone.gd to trigger landing animation
-	if landing:
-		animated_sprite.play("jump_land")
-		landing = false
-	else:
-		if abs(input_axis) > 0 or jumping:
-			if jumping:
-				animated_sprite.play("jump")
-			else:
-				animated_sprite.play("run")
-			if input_axis > 0:
-				animated_sprite.flip_h = false
-			if input_axis < 0:
-				animated_sprite.flip_h = true
+	if abs(input_axis) > 0 or jumping:
+		if jumping:
+			animated_sprite.play("jump")
 		else:
-			animated_sprite.play("idle")
+			animated_sprite.play("run")
+		if input_axis > 0:
+			animated_sprite.flip_h = false
+		if input_axis < 0:
+			animated_sprite.flip_h = true
+	else:
+		animated_sprite.play("idle")
 
 func get_input_axis():
 	var input_axis := Input.get_axis("left", "right")
 	return input_axis
 
 func accelerate(axis):
-	velocity.x = move_toward(velocity.x, axis * SPEED, ACCELERATION)
+	velocity.x = move_toward(velocity.x, axis * movement_data.speed, movement_data.acceleration)
 		
+func air_acceleration(axis):
+	velocity.x = move_toward(velocity.x, axis * movement_data.speed, movement_data.air_acceleration)
+	
 func friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, movement_data.friction)
 
 func run():
 	if !jumping:
 		animated_sprite.play("run")
+
+func jump():
+	velocity.y = movement_data.jump_velocity
+	jumping = true
+		
+func double_jump():
+	velocity.y = movement_data.jump_velocity * movement_data.double_jump_velocity
+	jumping = true
+	can_double_jump = false
+
+func manage_coyote_timer(just_left_floor):
+	if just_left_floor and velocity.y >= 0:
+		coyote_jump_timer.start()
+	if coyote_jump_timer.time_left > 0:
+		print("Coyote jump active!!!")
+	if jumping or is_on_floor():
+		coyote_jump_timer.stop()
+
+func manage_landing():
+	if landing:
+		print("landing!")
+		animated_sprite.play("jump_land")
+		landing_timer.start()
+		if landing_timer.timeout:
+			landing = false
